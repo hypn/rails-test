db6b16dc2132798c0d92a1697b9db3034243ad97
Ross Simpson <ross.simpson@gmail.com>
2019-02-09 17:47:59 +0200

    Result of running "rails g migration Cartridge colour:string" and "rake db:migrate"

---
+++ nes-games/db/migrate/20190209154734_cartridge.rb
@@ -0,0 +1,4 @@
+class Cartridge < ActiveRecord::Migration
+  def change
+  end
+end
--- nes-games/db/schema.rb
+++ nes-games/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190209152057) do
+ActiveRecord::Schema.define(version: 20190209154734) do
 
   create_table "cartridges", force: :cascade do |t|
     t.string   "title"
